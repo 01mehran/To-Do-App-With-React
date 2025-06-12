bfd858dce69e9986c7012ba4faa3278cb2c0188c
mehran <mehranmohamadi1311@gmail.com>
2025-06-13 03:08:29 +0330

    Add Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { SignUp } from "./pages/SignUp";
+
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <>
-      <SignUp />
+      <Home />
     </>
   );
 }
---
+++ src/components/BurgerIcon.jsx
@@ -0,0 +1,11 @@
+
+export const BurgerIcon = () => {
+  return (
+    <button className="flex h-6 w-6 cursor-pointer flex-col items-center justify-evenly lg:hidden mx-6 my-6">
+      <span className="h-[3px] w-full rounded-[2px] bg-black"></span>
+      <span className="h-[3px] w-full rounded-[2px] bg-black"></span>
+      <span className="h-[3px] w-full rounded-[2px] bg-black"></span>
+      <span className="h-[3px] w-full rounded-[2px] bg-black"></span>
+    </button>
+  );
+};
